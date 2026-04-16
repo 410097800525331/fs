e01bce31e2a675bc721ce1bb7dcc05254679939d
410097800525331 <xxx696193@gmail.com>
2026-04-16 13:06:04 +0900

    python

--- py_basic/ch06_입출력/ex01_표준입력.py
+++ py_basic/ch06_입출력/ex01_표준입력.py
@@ -1,9 +1,9 @@
 # 표준 입력
 # input()
 # int(): 문자열을 정수로 변환
-a = input("나이를 입력하세요: ")
-print(type(a)) # <class 'str'>
-print("30년 후 나이는 %d입니다" % (int(a) + 30))
+age = input("나이를 입력하세요: ")
+print(type(age)) # <class 'str'>
+print("30년 후 나이는 %d입니다" % (int(age) + 30))
 
 # 실수로 변환
 # float(): 문자열을 실수로 변환
@@ -11,3 +11,4 @@ print("30년 후 나이는 %d입니다" % (int(a) + 30))
 height = float(input("키를 입력하세요(cm): "))
 height /= 100
 print("미터로 환산하면 %.2f(m)입니다" % height)
+
---
+++ py_basic/ch06_입출력/ex05_with문.py
@@ -0,0 +1,5 @@
+
+
+# whit 문
+with open("foo.txt", "w") as f:
+  f.write("Life is too short, you need python")
