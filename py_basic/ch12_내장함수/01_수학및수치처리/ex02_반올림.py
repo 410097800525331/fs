d15f7c940bec39b3a702bd40b8050f811b8830f5
410097800525331 <xxx696193@gmail.com>
2026-04-23 14:01:49 +0900

    python

---
+++ py_basic/ch12_내장함수/01_수학및수치처리/ex02_반올림.py
@@ -0,0 +1,16 @@
+# round
+# round(number [,ndigits])는 숫자를 입력받아 반올림해 반환
+print(round(4.6)) # 5
+print(round(4.2)) # 4
+print(round(2.5)) # 2
+print(round(3.5)) # 4
+print(round(5.678, 2)) # 5.68
+
+# 자동 임포트: ctrl + .
+import math
+
+n = 3.556
+
+print(round(n, 2))   # 3.56 (소수점 둘째자리까지 반올림)
+print(math.ceil(n))   # 4
+print(math.floor(n))  # 3
